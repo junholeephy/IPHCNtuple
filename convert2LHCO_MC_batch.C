61c297482d2d297bd8189567ced9b49fb275d0f2
Anne-Catherine <anne-catherine.le.bihan@cern.ch>
2015-06-05 15:30:49 +0200

    submit root in batch mode

---
+++ convert2LHCO_MC_batch.C
@@ -0,0 +1,6 @@
+{
+
+gROOT->LoadMacro("convert2LHCO_MC.C");
+gROOT->ProcessLine("convert2LHCO_MC(\"/opt/sbg/data/sbgse24/data1/cms/lebihan/treeNicolas/output_TTZMadgraph.root\",\"input_ttZ_pmp.txt\",5)");
+
+}
