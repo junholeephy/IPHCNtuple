5162b0f68fd05c386a57e87a5a82e437dfe4b0cc
kskovpen <Kirill.Skovpen@cern.ch>
2015-06-08 19:45:45 +0200

    updated

--- NtupleProducer/test/run.zsh
+++ NtupleProducer/test/run.zsh
@@ -1,11 +1,8 @@
 #!/bin/env zsh
 
-#export LD_PRELOAD=/usr/lib64/libglobus_gssapi_gsi.so.4
-
 cdir=$(pwd)/../
 export LD_LIBRARY_PATH=${cdir}:$LD_LIBRARY_PATH
 
-#infl="lists/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_ID0.txt"
 infl="input.txt"
 
 ./NtupleProducer \
