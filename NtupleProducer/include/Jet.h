be744a2eeca31c74844f39a0185715df1aa2878d
Anne-Catherine <anne-catherine.le.bihan@cern.ch>
2015-11-19 14:12:35 +0100

    rm jet_met :-)

--- NtupleProducer/include/Jet.h
+++ NtupleProducer/include/Jet.h
@@ -32,28 +32,29 @@ class Jet : public Base
 
         float CSVv2()    {return _CSVv2;};
 
-        float metpt()        {return _metpt;};
+        /*
+	float metpt()      {return _metpt;};
         float metphi()       {return _metphi;};
-        float metsumet()     {return _metsumet;};
+        float metsumet()     {return _metsumet;};*/
 
         float jet_partonFlavour() {return _jet_partonFlavour  ;};
         float jet_hadronFlavour() {return _jet_hadronFlavour  ;};
 
         float jet_genJet_pt()     {return  _jet_genJet_pt    ;};
-        float jet_genJet_eta()    {return  _jet_genJet_eta   ;};
-        float jet_genJet_phi()    {return  _jet_genJet_phi   ;};
-        /*float jet_genJet_m()      {return  _jet_genJet_m   ;};
+        /*float jet_genJet_eta()    {return  _jet_genJet_eta ;};
+          float jet_genJet_phi()    {return  _jet_genJet_phi ;};
+          float jet_genJet_m()      {return  _jet_genJet_m   ;};
           float jet_genJet_E()      {return  _jet_genJet_E   ;};
           float jet_genJet_status() {return  _jet_genJet_status ;};
           float jet_genJet_id()     {return  _jet_genJet_id  ;};*/
 
         float jet_genParton_pt()     {return  _jet_genParton_pt     ;};
-        float jet_genParton_eta()    {return  _jet_genParton_eta    ;};
-        float jet_genParton_phi()    {return  _jet_genParton_phi    ;};
-        /*float jet_genParton_m()      {return  _jet_genParton_m      ;};
+        /*float jet_genParton_eta()    {return  _jet_genParton_eta    ;};
+          float jet_genParton_phi()    {return  _jet_genParton_phi    ;};
+          float jet_genParton_m()      {return  _jet_genParton_m      ;};
           float jet_genParton_E()      {return  _jet_genParton_E      ;};
-          float jet_genParton_status() {return  _jet_genParton_status ;};
-          float jet_genParton_id()     {return  _jet_genParton_id     ;};*/
+          float jet_genParton_status() {return  _jet_genParton_status ;};*/
+          float jet_genParton_id()     {return  _jet_genParton_id     ;};
 
         void  read();
         void  init();
@@ -75,28 +76,25 @@ class Jet : public Base
 
         float _CSVv2;
 
-        float _metpt;
-        float _metphi;
-        float _metsumet;
 
         float _jet_partonFlavour  ;
         float _jet_hadronFlavour  ;
 
         float _jet_genJet_pt      ;
-        float _jet_genJet_eta     ;
-        float _jet_genJet_phi     ;
-        /*float _jet_genJet_m       ;
+        /*float _jet_genJet_eta     ;
+          float _jet_genJet_phi     ;
+          float _jet_genJet_m       ;
           float _jet_genJet_E       ;
           float _jet_genJet_status  ;
           float _jet_genJet_id      ;*/
 
         float _jet_genParton_pt     ;
-        float _jet_genParton_eta    ;
-        float _jet_genParton_phi    ;
-        /*float _jet_genParton_m      ;
+        /*float _jet_genParton_eta    ;
+          float _jet_genParton_phi    ;
+          float _jet_genParton_m      ;
           float _jet_genParton_E      ;
-          float _jet_genParton_status ;
-          float _jet_genParton_id     ;*/
+          float _jet_genParton_status ;*/
+          float _jet_genParton_id     ;
 
         ClassDef(Jet,1)
 };
