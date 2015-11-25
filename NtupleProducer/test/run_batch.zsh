c9397be32a3a1a806be08d1cd9f4ee100527265f
Anne-Catherine <anne-catherine.le.bihan@cern.ch>
2015-11-25 18:03:40 +0100

    Kirill's scripts to run on pbs

---
+++ NtupleProducer/test/run_batch.zsh
@@ -0,0 +1,85 @@
+#!/bin/env zsh
+
+cp /tmp/x509up_u6155 /home-pbs/lebihan/someone/proxy/.
+
+jName=${1}
+if [[ ${jName} == "" ]]; then
+  echo "Please specify the run name"
+  exit 1
+fi
+
+que="sbg_local"
+
+export HOME=$(pwd)
+
+dout="/home-pbs/lebihan/someone/ttH_22112015_MC/ttH/NtupleProducer/test/"
+
+
+runName="toy${jName}"
+logName="log${jName}"
+
+rm -rf ${runName}
+mkdir ${runName}
+rm -rf ${logName}
+mkdir ${logName}
+
+nmax=-1
+
+fxsec="table.txt"
+
+fdir=$(ls -d lists*)
+
+echo $fdir
+
+echo $fdir | while read line
+do
+fpath="${HOME}/${line}/"
+flist=$(ls ${fpath})
+dir=${line}
+
+echo $flist | while read line
+do
+  jidx=0
+  sample=$(echo $line | sed 's%.txt%%g')
+  dataset=$(echo $sample | sed 's%_ID..*%%g')
+  if [[ ! -d ${runName}/${dataset} ]]; then
+    mkdir ${runName}/${dataset}
+  fi
+  linexsec=$(grep $dataset $fxsec)
+  noe=$(echo $linexsec | awk '{print $3}')
+  xsec=$(echo $linexsec | awk '{print $2}')
+  if [[ $noe == "" ]]; then
+    noe=1
+  fi
+  if [[ $xsec == "" ]]; then
+    xsec=1
+  fi
+  fl=$(echo $sample | cut -c1-1)
+  fl2=$(echo $sample | cut -c1-11)
+  datamc=""
+  if [[ $fl == "J" ]]; then
+    isdata=1
+    datamc="DATA"
+    nmax=${nmax}
+  else
+    isdata=0
+    datamc="MC"
+    nmax=${nmax}
+  fi
+  
+  isdata=0
+
+  fout=$(echo ${runName}/${dataset}/${line}_${jidx} | sed 's%.txt%%g')
+  lout=$(echo ${line}_${jidx} | sed 's%.txt%%g')
+
+# echo "${dataset}: $noe $xsec"
+  echo "${fpath}${line}"
+ 
+  qsub -N ${dir} -q ${que} -o ${logName}/${sample}.log -j oe single_batch_job.sh \
+-v dout=${dout},line2=${fpath}${line},fout=${fout},noe=${noe},xsec=${xsec},isdata=${isdata},sample=${sample},nmax=${nmax}
+done
+
+echo "going to sleep 2700 s (45 mn)"
+sleep 2700
+
+done 
---
+++ NtupleProducer/test/single_batch_job.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export X509_USER_PROXY=/home-pbs/lebihan/someone/proxy/x509up_u6155
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+cd /home-pbs/lebihan/someone/CMSSW_7_4_12_patch4/src
+export SCRAM_ARCH=slc6_amd64_gcc491
+eval `scramv1 runtime -sh`
+cd -
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dout}/../
+
+line2=${line2}
+fout=${fout}
+isdata=${isdata}
+noe=${noe}
+xsec=${xsec}
+dout=${dout}
+sample=${sample}
+
+
+echo "Executing .././NtupleProducer --file ${line2} --outfile ${dout}${fout} --isdata ${isdata} --noe ${noe} --xsec ${xsec} --nmax ${nmax}"
+${dout}/./NtupleProducer --file ${line2} --outfile ${dout}${fout} --isdata ${isdata} --noe ${noe} --xsec ${xsec} --nmax ${nmax} --tree FlatTree/tree
