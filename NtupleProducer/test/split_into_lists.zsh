79d3aae870cd08e158db360c665f3c5f9ea74bf0
Anne-Catherine <anne-catherine.le.bihan@cern.ch>
2016-02-02 10:38:09 +0100

    Bbug fix for subdir from Kirill

--- NtupleProducer/test/split_into_lists.zsh
+++ NtupleProducer/test/split_into_lists.zsh
@@ -1,17 +1,17 @@
 #!/bin/env zsh
 
-fpath="/dpm/in2p3.fr/home/cms/phedex/store/user/kskovpen/FlatTree/MantaRay-patch9-v20151110/"
+fpath="/dpm/in2p3.fr/home/cms/phedex/store/user/kskovpen/FlatTree/Akoula-patch2-v20160128//"
 
-dataStr="SingleElectron|SingleMuon|DoubleEG|DoubleMuon|MuonEG|BTagMu"
+dataStr="SingleElectron|SingleMuon|DoubleEG|DoubleMuon|MuonEG"
 
-liDATA=$(/usr/bin/rfdir ${fpath} | egrep -e "$dataStr")
-liMC=$(/usr/bin/rfdir ${fpath} | egrep -v "$dataStr")
+liDATA=($(/usr/bin/rfdir ${fpath} | egrep -e "$dataStr" | awk '{print $9}'))
+liMC=($(/usr/bin/rfdir ${fpath} | egrep -v "$dataStr" | awk '{print $9}'))
 
 fpathDATAXRD=$(echo ${fpath} | sed "s%/dpm%root://sbgse1.in2p3.fr//dpm%g")
 fpathMCXRD=$(echo ${fpath} | sed "s%/dpm%root://sbgse1.in2p3.fr//dpm%g")
 
-nFilesDATA=20 # per job
-nFilesMC=5
+nFilesDATA=30 # per job
+nFilesMC=10
 outDir="lists/"
 
 rm -rf ${outDir}
@@ -20,28 +20,31 @@ mkdir ${outDir}
 # DATA
 
 rm -f /tmp/tempDATA.txt
-echo $liDATA | while read line
+for line in $liDATA
 do
-  d1=$(echo $line | awk '{print $9}')
-  liDATA2=$(/usr/bin/rfdir ${fpath}${d1})
-  d2=$(echo $liDATA2 | awk '{print $9}')
-  echo $d2 | while read id2
+  echo $line
+  d1=$(echo $line)
+  liDATA2=$(/usr/bin/rfdir ${fpath}${d1} | awk '{print $9}')
+  d2=($(echo $liDATA2))
+  for id2 in $d2
   do
     liDATA3=$(/usr/bin/rfdir ${fpath}${d1}/${id2})
     d3=$(echo $liDATA3 | awk '{print $9}')
-    liDATA4=$(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3})
-    d4=$(echo $liDATA4 | awk '{print $9}')
-    liDATA5=$(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3}/${d4})
-    echo $liDATA5 | while read line2
+    liDATA4=($(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3} | awk '{print $9}'))
+    for d4 in $liDATA4
     do
-      f1=$(echo $line2 | awk '{print $9}')
-      file=$(echo ${fpathDATAXRD}${d1}/${id2}/${d3}/${d4}/${f1})
-      echo "${file}" >> /tmp/tempDATA.txt
+      liDATA5=($(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3}/${d4} | awk '{print $9}'))
+      for line2 in $liDATA5
+      do
+        f1=$(echo $line2)
+        file=$(echo ${fpathDATAXRD}${d1}/${id2}/${d3}/${d4}/${f1})
+        echo "${file}" >> /tmp/tempDATA.txt
+      done
     done
     split -a 5 -l ${nFilesDATA} -d /tmp/tempDATA.txt /tmp/${d1}_
-    lsfi=$(ls /tmp/${d1}_*)
+    lsfi=($(ls /tmp/${d1}_*))
     jid=0
-    echo $lsfi | while read fil
+    for fil in $lsfi
     do
       if [[ $#d2 != 1 ]]; then
         mv ${fil} ${outDir}${d1}_${id2}_ID${jid}.txt
@@ -57,31 +60,35 @@ done
 # MC
 
 rm -f /tmp/tempMC.txt
-echo $liMC | while read line
+for line in $liMC
 do
-  d1=$(echo $line | awk '{print $9}')
-  liMC2=$(/usr/bin/rfdir ${fpath}${d1})
-  d2=$(echo $liMC2 | awk '{print $9}')
-  echo $d2 | while read id2
+  echo $line
+  d1=$(echo $line)
+  liMC2=$(/usr/bin/rfdir ${fpath}${d1} | awk '{print $9}')
+  d2=($(echo $liMC2))
+  for id2 in $d2
   do
     liMC3=$(/usr/bin/rfdir ${fpath}${d1}/${id2})
     d3=$(echo $liMC3 | awk '{print $9}')
-    liMC4=$(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3})
-    d4=$(echo $liMC4 | awk '{print $9}')
-    liMC5=$(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3}/${d4})
-    echo $liMC5 | while read line2
+    liMC4=($(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3} | awk '{print $9}'))
+    for d4 in $liMC4
     do
-      f1=$(echo $line2 | awk '{print $9}')
-      file=$(echo ${fpathMCXRD}${d1}/${id2}/${d3}/${d4}/${f1})
-      echo "${file}" >> /tmp/tempMC.txt
+      liMC5=($(/usr/bin/rfdir ${fpath}${d1}/${id2}/${d3}/${d4} | awk '{print $9}'))
+      for line2 in $liMC5
+      do
+        f1=$(echo $line2)
+        file=$(echo ${fpathMCXRD}${d1}/${id2}/${d3}/${d4}/${f1})
+        echo "${file}" >> /tmp/tempMC.txt
+      done
     done
     split -a 5 -l ${nFilesMC} -d /tmp/tempMC.txt /tmp/${d1}_
-    lsfi=$(ls /tmp/${d1}_*)
+    lsfi=($(ls /tmp/${d1}_*))
     jid=0
-    echo $lsfi | while read fil
+    for fil in $lsfi
     do
+      sampStrip=$(echo $id2 | sed "s%_RunIISpring15MiniAODv2_.*%%g")
       if [[ $#d2 != 1 && ${d1} != "tHFCNC13TeV" ]]; then
-        mv ${fil} ${outDir}${d1}_${id2}_ID${jid}.txt
+        mv ${fil} ${outDir}${d1}_${sampStrip}_ID${jid}.txt
       else
         if [[ ${d1} == "tHFCNC13TeV" ]]; then
           mv ${fil} ${outDir}${d1}"_"$(echo ${id2} | sed "s%_AODFASTSIM_.*%%g")_ID${jid}.txt
@@ -94,3 +101,4 @@ do
     rm -f /tmp/tempMC.txt
   done
 done
+
