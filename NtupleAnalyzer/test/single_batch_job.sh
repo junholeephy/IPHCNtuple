68de61e9a7a49e0285ed99afc559c9110fd9262e
kskovpen <Kirill.Skovpen@cern.ch>
2016-03-22 09:44:29 +0100

    update

--- NtupleAnalyzer/test/single_batch_job.sh
+++ NtupleAnalyzer/test/single_batch_job.sh
@@ -2,13 +2,13 @@
 
 export X509_USER_PROXY=/home-pbs/nchanon/proxy/x509up_u8148
 
-source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /cvmfs/cms.cern.ch/cmsset_default.shx
 cd /home-pbs/nchanon/CMSSW_7_4_12_patch4/src
 export SCRAM_ARCH=slc6_amd64_gcc491
 eval `scramv1 runtime -sh`
 cd -
 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dout}/../:${dout}/../../NtupleProducer/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${dout}/../:${dout}/../../NtupleProducer/:${dout}/../../../KinFit/
 
 
 echo ${xsec}
