64f521fe799a9d796f571dc5bf4cded1aef57103
Xavier COUBEZ <xavier.coubez@cern.ch>
2016-06-09 17:49:06 +0200

    Updating Charge Flip and Fake Rate - Functions for Fake Rate application to be completed

--- NtupleAnalyzer/src/ChargeFlip.cxx
+++ NtupleAnalyzer/src/ChargeFlip.cxx
@@ -19,7 +19,7 @@ double get_QF_wgt_2l( std::vector<double> leptonsPts,
 {
 
     double weight    = 1;
-    double weight_QF = 1;
+    double weight_QF = 0;
 
     for( int ilepton=0; ilepton<int(leptonsPts.size()); ilepton++ )
     {
@@ -35,14 +35,12 @@ double get_QF_wgt_2l( std::vector<double> leptonsPts,
         if      ( leptonEta < 1.479 ) iEta = 1;
         else if ( leptonEta < 2.5   ) iEta = 2;
 
-        weight_QF = h_QF_wgt->GetBinContent(iPt,iEta); 
-        weight    = weight * weight_QF;
+        weight_QF = weight_QF + h_QF_wgt->GetBinContent(iPt,iEta); 
  
         //std::cout << "weight from charge flip : " << weight << std::endl;
-
-        //
     }
 
+    weight    = weight * weight_QF;
     return weight;
 }
 
--- NtupleAnalyzer/src/FakeRate.cxx
+++ NtupleAnalyzer/src/FakeRate.cxx
@@ -28,6 +28,62 @@ double get_FR_wgt_2l( std::vector<double> leptonsPts,
                       std::vector<double> leptonsEtas,
                       std::vector<int>    leptonsId)
 {
+    double weight    = 1;
+    double weight_FR = 1;
+
+    if( leptonsPts.size() == 1)
+    {
+        int iPt, iEta;
+        double leptonPt  = leptonsPts[0];
+        double leptonEta = fabs( leptonsEtas[0] );
+        int    leptonId  = abs(  leptonsId[0]   );
+
+        if      ( leptonPt < 25 ) iPt = 1;
+        else if ( leptonPt < 30 ) iPt = 2;
+        else if ( leptonPt < 40 ) iPt = 3;
+        else if ( leptonPt < 40 ) iPt = 4;
+        else                      iPt = 5;
+
+        if      ( leptonEta < 1.479 ) iEta = 1;
+        else if ( leptonEta < 2.5   ) iEta = 2;
+        
+        if(leptonId == 11)
+        {
+            double f1 = h_FR_wgt_el->GetBinContent(iPt,iEta);
+            weight_FR = f1 / (1-f1);
+        }
+        else if (leptonId == 13)
+        {
+            double f1 = h_FR_wgt_mu->GetBinContent(iPt,iEta);
+            weight_FR = f1/ (1-f1);
+        }
+
+        weight    = weight * weight_FR;
+
+        //std::cout << "weight from charge flip : " << weight << std::endl;
+    }
+    else
+    {
+        int iPt0, iEta0, iPt1, iEta1;
+        double leptonPt0  = leptonsPts[0];
+        double leptonPt1  = leptonsPts[1];
+        double leptonEta0 = fabs( leptonsEtas[0] );
+        double leptonEta1 = fabs( leptonsEtas[1] );
+        int    leptonId0  = abs(  leptonsId[0]   );
+        int    leptonId1  = abs(  leptonsId[1]   );
+
+
+
+
+    }
+
+    return weight;
+}
+
+double get_FR_wgt_3l( std::vector<double> leptonsPts,
+                      std::vector<double> leptonsEtas,
+                      std::vector<int>    leptonsId)
+{
 
     double weight;
 
@@ -39,4 +95,7 @@ double get_FR_wgt_2l( std::vector<double> leptonsPts,
 
     return weight;
 }
+
+
+
 
